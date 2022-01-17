2b7c5205a4c7c3a469de950ffea290d4339c092b
mmaider <moiseevasd@gmail.com>
2022-01-17 10:53:46 +0300

    Стартовая страничка тест

--- main.py
+++ main.py
@@ -190,6 +190,64 @@ def blit_hp(surf, x, y, pct):
     pygame.draw.rect(surf, WHITE, outline_rect, 2)
 
 
+def main_menu():
+    global running, menu_running, rules_running, screen, WIDTH, HEIGHT
+    intro_text = ['THE FLOORS']
+    buttons = ['Начать игру']
+    rules = ['Правила игры']
+    fon = pygame.transform.scale(load_image('main_back.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.SysFont('verdana', 35)
+    text_coord = 30
+    for line in intro_text:
+        string_rendered = font.render(line, 1, (255, 255, 255))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    font = pygame.font.SysFont('verdana', 20)
+    for line in rules:
+        string_rendered = font.render(line, 1, (255, 255, 255))
+        intro_rect1 = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect1.top = text_coord
+        intro_rect1.x = 10
+        print(intro_rect1)
+        text_coord += intro_rect1.height
+        screen.blit(string_rendered, intro_rect1)
+
+    font = pygame.font.SysFont('serif', 17)
+    for line in buttons:
+        string_rendered = font.render(line, 1, (255, 255, 255))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        print(intro_rect)
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while menu_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu_running = False
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+        pygame.display.flip()
+        clock.tick(20)
+
+
+def rules_menu():
+    print(1)
+
+def main_cycle():
+    global running
+    print(1)
+
+
 bgimg = pygame.transform.scale(load_image("bg.png"), (WIDTH, HEIGHT))
 playerimage = pygame.transform.scale(load_image("playerimg.png"), (576, 256))
 mobimage = pygame.transform.scale(load_image("mob.png"), (282, 190))
@@ -206,40 +264,16 @@ for i in range(3):
     mobs.add(m)
 
 running = True
+menu_running = True
+rules_running = False
+
 while running:
     clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                player.shoot(mouse_x, mouse_y)
-    screen.blit(bgimg, (0, 0))
-    all_sprites.update(player.rect.x, player.rect.y)
-
-    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
-    for hit in hits:
-        m = Mob(mobimage, 6, 4)
-        all_sprites.add(m)
-        mobs.add(m)
-
-    hits = pygame.sprite.spritecollide(player, mobs, True)
-    if hits:
-        player.life -= 1
-        m = Mob(mobimage, 6, 4)
-        all_sprites.add(m)
-        mobs.add(m)
-
-    if player.life == 0:
-        running = False
-    font = pygame.font.Font(None, 32)
-    all_sprites.draw(screen)
-    text = font.render(
-        str(player.life), True, WHITE)
-    place = text.get_rect(center=(50, 30))
-    screen.blit(text, place)
-    blit_hp(screen, 5, 5, player.life)
-    pygame.display.flip()
+    if menu_running:
+        main_menu()
+    elif rules_running:
+        rules_menu()
+    else:
+        main_cycle()
 
 pygame.quit()
